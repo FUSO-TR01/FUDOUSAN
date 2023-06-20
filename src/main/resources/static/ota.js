95d1dfa7b09cfb8a9d94dcedda97372a92825aa6
FUSO-TR01 <fusotr001@gmail.com>
2023-06-20 16:03:08 +0900

    17c

---
+++ src/main/resources/static/ota.js
@@ -0,0 +1,18 @@
+function modalOpen() {
+			//------------------------------------------------------------
+			//  モーダルウインドウ オープン
+			//------------------------------------------------------------
+			document.getElementById("modalArea").className = "modalBg modalBgOpen";
+		}
+
+		function modalClose() {
+			//------------------------------------------------------------
+			//  モーダルウインドウ クローズ
+			//------------------------------------------------------------
+			document.getElementById("modalArea").className = "modalBg modalBgClose";
+		}
+		
+//		_________
+		
+	
+		
