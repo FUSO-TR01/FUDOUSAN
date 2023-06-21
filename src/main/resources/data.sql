f86dd69b19c72bf382d0d9077c4cc29befbb0ec7
fusotr002_3320 <fusotr002@gmail.com>
2023-06-21 13:12:27 +0900

    データ量追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
 INSERT INTO home (name,space,money,address,comment) VALUES 
 ('でかマンション','1LDK',50000,'東京都','トイレなし'),
 ('でかでかマンション','1R',50000,'大阪府','Wi-Fiなし'),
-('ちびマンション','1DK',40000,'北海道','ペット可');
+('ちびマンション','1DK',40000,'北海道','ペット可'),
+('もりもりマンション','1LDK',150000,'大阪府','無料Wi-Fiあり,駐車場空きあり,ペット可,室内洗濯機置き場あり,オートロックあり,バス・トイレ別');
 
 INSERT INTO login(logId,pass,type,name) VALUES 
 ('ID','Password','業者名','名前'),
